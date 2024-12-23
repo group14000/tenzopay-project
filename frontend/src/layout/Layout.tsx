38519973b0f030189954b03f5bb5486cf1d04384
diganta14000 <diganta.halder@framedx.com>
2024-12-23 12:47:40 +0530

    frontend added

---
+++ frontend/src/layout/Layout.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react';
+import Sidebar from './Sidebar';
+import Header from './Header';
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+        <div className="flex h-screen overflow-hidden">
+            <div className="h-screen">
+                <Sidebar />
+            </div>
+            <div className="flex flex-col flex-grow h-screen overflow-hidden">
+                <Header />
+                <main className="flex-grow p-4 overflow-auto">
+                    {children}
+                </main>
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
